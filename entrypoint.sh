64e61aa964970afc83141be191eb34d2df449709
DuocDVT <thanhduoc020919@gmail.com>
2026-01-15 11:06:03 +0700

    Fix Livewire 404

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+echo "== Laravel booting =="
+
 php artisan key:generate --force
 
 php artisan migrate --force || true
 
 php artisan livewire:publish --force
+
+php artisan filament:assets || true
 
 php artisan optimize:clear
 
